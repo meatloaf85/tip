549b36507be08197f7b1eab71c9fa96a12a556b8
jar <T@FPg4G!iFfK!e66>
2020-12-07 12:01:22 -0700

    added slider feature and settings view

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     
+
+    @IBOutlet weak var slideAmount: UISlider!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipPercentageLabel: UILabel!
@@ -23,14 +25,24 @@ class ViewController: UIViewController {
     @IBAction func onTap(_ sender: Any) {
     }
     
+    
+    
     @IBAction func calculateTip(_ sender: Any) {
         // Get initial bill amount and calculate tip
         let bill = Double(billAmountTextField.text!) ?? 0
         
-        let tipPercentages = [0.15, 0.2, 0.25]
+        let tipPercentages = [0.0,0.15, 0.2, 0.25]
+        
+        let slider = Double(slideAmount.value)
         
         // Calculate tip and total
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        var tip =  bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        if (slider != 0){
+            tip = Double(slideAmount.value) * bill
+        }
+        
         let total = bill + tip
         
         // Update the tip and total labels
